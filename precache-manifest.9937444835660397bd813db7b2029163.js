d12951bf586b0833879e6f2a8de1ba25f576b287
darmun <darmun@protonmail.com>
2019-03-11 16:49:28 +0100

    Updates

---
+++ precache-manifest.9937444835660397bd813db7b2029163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b122621b1a3d160945d",
+    "url": "/final-countdown/static/js/runtime~main.8b122621.js"
+  },
+  {
+    "revision": "184d073fd11d0302519d",
+    "url": "/final-countdown/static/js/main.184d073f.chunk.js"
+  },
+  {
+    "revision": "d6bb29969a014ea86114",
+    "url": "/final-countdown/static/js/2.d6bb2996.chunk.js"
+  },
+  {
+    "revision": "d6bb29969a014ea86114",
+    "url": "/final-countdown/static/css/2.4007eede.chunk.css"
+  },
+  {
+    "revision": "d93767d4ea753a499e849daca2f9a35a",
+    "url": "/final-countdown/index.html"
+  }
+];
--- src/Components/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Countdown from './Countdown';
-
-export default () => 
-        <Countdown />
-
-
