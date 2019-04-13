bfe3068761878d40f6ef03c02fc03633ccfea61d
Darmun <35923418+Darmun@users.noreply.github.com>
2019-04-13 16:22:59 +0200

    Update index.js
    Updated placeholder description

--- src/Components/Datepicker/index.js
+++ src/Components/Datepicker/index.js
@@ -37,7 +37,7 @@ export default class extends React.Component {
           <input
             className="input is-medium is-rounded"
             value={date}
-            placeholder="Pick a date.."
+            placeholder="Pick a date [MM-DD-YYYY]"
             onChange={this.handleDateChange}
           />
         </p>
